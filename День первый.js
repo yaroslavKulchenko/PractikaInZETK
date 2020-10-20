dfcfbabe348a6101446aed351bd57b90875f414e
yaroslavKulchenko <perekatipole123123@gmail.com>
2020-10-21 02:22:43 +0300

    mission comlete

---
+++ День первый.js
@@ -0,0 +1,25 @@
+function cikli1(){
+    for(let i = 4; i <= 400; i++){
+        console.log(i);
+    }
+}
+
+function cikli3(){
+    for(let i = 654; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+function cikli4(){
+    for(let i = 1983; i <= 2017; i++){
+        console.log(i);
+    }
+}
+
+function cikli5(){
+    for (let i = -4; i <= 100; i++){
+        if (i % 2 == 0 ) {
+            console.log(i);
+        }
+    }
+}
